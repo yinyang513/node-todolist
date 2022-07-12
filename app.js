21841960e04e8bf2a21f01847a1bebc4b2d940e4
Arnav <arnavguneta@gmail.com>
2022-07-12 00:45:54 -0400

    fixed

--- app.js
+++ app.js
@@ -1,81 +1,40 @@
-// const createError = require('http-errors');
-// const express = require('express');
-// const path = require('path');
-// const cookieParser = require('cookie-parser');
-// const logger = require('morgan');
-// const bodyParser = require('body-parser');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-// const app = express();
+const app = express();
 
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// app.use(logger('dev'));
-// // app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// // app.use(bodyParser.urlencoded({ extended: false }));
-// // app.use(bodyParser.json())
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// // Set content type GLOBALLY for any response.
-// app.use(function (req, res, next) {
-//   res.setHeader('Content-type','application/x-www-form-urlencoded');
-//   next();
-// });
-
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// module.exports = app;
-
-var express = require('express');
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer();
-var app = express();
-
-app.get('/', function(req, res){
-   res.render('index');
-});
-
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.set('views', './views');
 
-// for parsing application/json
-app.use(bodyParser.json()); 
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// for parsing application/xwww-
-app.use(bodyParser.urlencoded({ extended: true })); 
-//form-urlencoded
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-// for parsing multipart/form-data
-app.use(upload.array()); 
-app.use(express.static('public'));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.post('/', function(req, res){
-   console.log(req.body);
-   res.send("recieved your request!");
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
-app.listen(3000);
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
